9c2ec39b35f01365f01d2ad81b81a13cb48314d7
Cristian Romero Andrade <mascrit@gmail.com>
2021-02-02 01:44:02 -0600

    Se translada a proyecto umbrella

---
+++ apps/tiny_tysy/mix.exs
@@ -0,0 +1,42 @@
+defmodule TinyTysy.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :tiny_tysy,
+      version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.11",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {TinyTysy.Application, []}
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:nostrum, "~> 0.4"},
+      {:ecto_sql, "~> 3.0"},
+      {:postgrex, ">= 0.0.0"},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+    ]
+  end
+end
--- mix.exs
+++ mix.exs
@@ -1,33 +1,21 @@
-defmodule TinyTysy.MixProject do
+defmodule Tysy.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :tiny_tysy,
+      apps_path: "apps",
       version: "0.1.0",
-      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
-    [
-      extra_applications: [:logger],
-      mod: {TinyTysy.Application, []}
-    ]
-  end
-
-  # Run "mix help deps" to learn about dependencies.
+  # Dependencies listed here are available only for this
+  # project and cannot be accessed from applications inside
+  # the apps folder.
+  #
+  # Run "mix help deps" for examples and options.
   defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:nostrum, "~> 0.4"},
-      {:ecto_sql, "~> 3.0"},
-      {:postgrex, ">= 0.0.0"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
-    ]
+    []
   end
 end
